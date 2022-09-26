dbcddc99fd14195997be3be581ca37c8e0b0a784
Douglas Castro <douglashenriquees@gmail.com>
2022-09-26 17:11:01 -0300

    add repositories

--- Discount.Grpc/Program.cs
+++ Discount.Grpc/Program.cs
@@ -1,12 +1,11 @@
-using Discount.Grpc.Services;
+using Discount.Grpc.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGrpc();
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 var app = builder.Build();
-
-app.MapGrpcService<GreeterService>();
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 * ```dotnet new grpc -o solution_name.template_project --no-https true```
 * ```dotnet sln solution_name.sln add ./solution_name.template_project/solution_name.template_project.csproj```
 * ```dotnet new gitignore```
+* ```cd ./solution_name.template_project/solution_name.template_project.csproj```
+* ```dotnet add package Npgsql```
+* ```dotnet add package Dapper```
